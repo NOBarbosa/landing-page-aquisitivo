1e4a7a34e521deea9af83b1565fadedfb7025138
Natalia Barbosa <natalia.barbosa@zig.fun>
2024-07-14 13:58:08 -0300

    feat:add icons

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,35 +1,55 @@
 import s from "./Home.module.scss";
 import aquisi from "../../assets/aquisi.jpg";
+import { InstagramLogo, TiktokLogo, XLogo } from "@phosphor-icons/react";
 
 export function Home() {
   return (
     <main className={s.home}>
-      <img src={aquisi} alt="foto-do-aquisitivo" />
+      <div className={s.imgContainer}>
+        <img src={aquisi} alt="foto-do-aquisitivo" />
+      </div>
       <h1>Treinador</h1>
       <h4>@aquisitivo</h4>
+      <a
+        className={s.button}
+        href={"https://onlyfans.com/aquisitivo"}
+        target="_blank"
+      >
+        Onlyfans
+      </a>
+      <a
+        href="https://privacy.com.br/checkout/aquisitivo"
+        target="_blank"
+        className={s.button}
+      >
+        Privacy
+      </a>
+      <a
+        href="https://www.instagram.com/aquisitivo/"
+        target="_blank"
+        className={s.button}
+      >
+        Instagram
+      </a>
+      <a href="https://x.com/aquisitivo" target="_blank" className={s.button}>
+        X
+      </a>
 
-      <button className={s.button}>
-        <a href="https://onlyfans.com/aquisitivo" target="_blank">
-          Onlyfans
-        </a>
-      </button>
-      <button className={s.button}>
-        <a href="https://privacy.com.br/checkout/aquisitivo" target="_blank">
-          Privacy
-        </a>
-      </button>
-      <button className={s.button}>
-        <a href="https://www.instagram.com/aquisitivo/" target="_blank">
-          Instagram
-        </a>
-      </button>
-      <button className={s.button}>
+      <div className={s.icons}>
         <a href="https://x.com/aquisitivo" target="_blank">
-          X
+          <XLogo size={32} />
         </a>
-      </button>
+        <a href="https://www.instagram.com/aquisitivo/" target="_blank">
+          <InstagramLogo size={32} />
+        </a>
+        <a href="https://www.tiktok.com/@onlyaquisitivo" target="_blank">
+          <TiktokLogo size={32} />
+        </a>
+      </div>
 
-      <footer>© Aquisitivo. Todos os Direitos Reservados.</footer>
+      <footer className={s.footer}>
+        © Aquisitivo. Todos os Direitos Reservados.
+      </footer>
     </main>
   );
 }
